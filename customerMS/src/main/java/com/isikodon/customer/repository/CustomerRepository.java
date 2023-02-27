3793c7f5cef1d092fc7b19dea598fbbab1609baa
donpachi <isikodon@googlemail.com>
2023-02-27 10:59:56 +0000

    feat: Create customer microservice module with registration feature
    Add necessary dependencies and configuration to enable registering customers via a REST API. Specifically:
    - Add "starter-web", "data-jpa", and "postgres" dependencies
    - Write Docker Compose configuration for PostgreSQL and pgAdmin
    - Implement configuration in application.yaml to connect to PostgreSQL database
    - Create Customer entity class and CustomerRegistrationRequest class
    - Declare controller, service, and repository components
    - Implement customer registration business logic
    
    Also, add a banner text for the microservice to improve user experience.

---
+++ customerMS/src/main/java/com/isikodon/customer/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.isikodon.customer.repository;
+
+import com.isikodon.customer.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<CustomerEntity, Integer> {
+}
