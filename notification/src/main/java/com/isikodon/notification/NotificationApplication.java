b0ac93adf402fc5d2c3690111be1e953be2ceb0d
donpachi <isikodon@googlemail.com>
2023-03-08 15:42:35 +0000

    refactor: migrate microservices to k8s
    - disable eureka & api-gateway
    - refactor feign clients
    - explicitly declare spring profiles active on local IDE run configurations, and manifests
    - add kube profile to all microservices
    - package, build and push docker images
    - create manifests for notification, customer, fraud
    	- service.yml
        - deployment.yml

--- notification/src/main/java/com/isikodon/notification/NotificationApplication.java
+++ notification/src/main/java/com/isikodon/notification/NotificationApplication.java
@@ -2,11 +2,16 @@ package com.isikodon.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(scanBasePackages = {
         "com.isikodon.notification",
         "com.isikodon.amqp"
 })
+@PropertySources(
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
